88f1e9b6af05bfa0778f7b38451386a2d2707bef
LeeAnne Galioto <leeanne@helloample.com>
2018-11-09 22:58:26 -0500

    Editing a Todo extract the todo template into it's own component for cleaner code

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -1,36 +1,21 @@
 <template>
+  <!-- JavaScript expressions in Vue are enclosed in double curly brackets. -->
   <div>
-    <!-- JavaScript expressions in Vue are enclosed in double curly brackets. -->
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <div class='ui centered card' v-for="todo in todos">
-      <div class='content'>
-        <div class='header'>
-          {{ todo.title }}
-        </div>
-        <div class='meta'>
-          {{ todo.project }}
-        </div>
-        <div class='extra content'>
-          <span class='right floated edit icon'>
-            <i class='edit icon'></i>
-          </span>
-        </div>
-      </div>
-      <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-      </div>
-      <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-      </div>
-    </div>
+    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
+// import sweetalert from 'sweetalert';
+import Todo from './Todo';
 
 export default {
   props: ['todos'],
+  components: {
+    Todo,
+  },
 }
 </script>
 <style>
