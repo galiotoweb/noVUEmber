ab201840d7390dfc42e3c6baf9b7aa7e947fcb2f
LeeAnne Galioto <leeanne@helloample.com>
2018-11-10 00:22:29 -0500

    Deleting a Todo onclick "an event's emitted & propagated to the parent component which will then delete it."

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -3,7 +3,7 @@
   <div>
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
+    <todo  v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"/>
   </div>
 </template>
 
@@ -15,6 +15,12 @@ export default {
   props: ['todos'],
   components: {
     Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
   },
 };
 </script>
