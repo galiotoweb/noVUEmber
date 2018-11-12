7db0d6f9bec0478420445e5494a0d7a44f7bffc5
LeeAnne Galioto <leeanne@helloample.com>
2018-11-12 00:20:16 -0500

    Add images and product array

--- vue-router/src/components/home.vue
+++ vue-router/src/components/home.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="home">
+  <div class="container home">
     <h1>{{ title }}</h1>
+    <div class="row">
+      <div class="col-md-4 col-lg-4"
+      v-for="(data,index) in products"
+      :key="index">
+        <img :src="data.image" class="img-fluid">
+        <h3>{{data.productTitle}}</h3>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +18,39 @@ export default {
   data() {
     return {
       title: 'Home',
+      // https://source.unsplash.com/random
+      products: [
+        {
+          productTitle: 'ABC',
+          image: require('../assets/images/photo1.jpg'),
+          productId: 1,
+        },
+        {
+          productTitle: 'DEF',
+          image: require('../assets/images/photo2.jpg'),
+          productId: 2,
+        },
+        {
+          productTitle: 'GHI',
+          image: require('../assets/images/photo3.jpg'),
+          productId: 3,
+        },
+        {
+          productTitle: 'JKL',
+          image: require('../assets/images/photo4.jpg'),
+          productId: 4,
+        },
+        {
+          productTitle: 'MNO',
+          image: require('../assets/images/photo5.jpg'),
+          productId: 5,
+        },
+        {
+          productTitle: 'PQR',
+          image: require('../assets/images/photo6.jpg'),
+          productId: 6,
+        },
+      ],
     };
   },
 };
