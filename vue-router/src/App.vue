215980b3db574a149d7ec428e005bcd95236aea4
LeeAnne Galioto <leeanne@helloample.com>
2018-11-11 22:27:28 -0500

    Add navbar and Bootswatch theme

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- navbar -->
+    <div class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+      <div class="container">
+      <router-link to="/" class="navbar-brand">Home</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../blog/">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../contact/">Contact</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -13,11 +37,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ vue-router/src/components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Vue Router Project Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'A NoVUEmber App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .home {
+    padding-top: 100px;
+    text-align: center;
+  }
+</style>
